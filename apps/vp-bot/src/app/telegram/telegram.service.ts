20b8bbbfbde33fd39002f36995ac93adb79a27f2
Franek Marciniak <prostyfranek@protonmail.com>
2023-12-22 16:38:05 +0100

    cleanup

--- apps/vp-bot/src/app/telegram/telegram.service.ts
+++ apps/vp-bot/src/app/telegram/telegram.service.ts
@@ -1,8 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TelegramService {
-  testBot(): { message: string } {
-    return { message: 'Hello Telegram' };
-  }
-}
+export class TelegramService {}
